9c1be722120eb35cb7fac24bf1194b8d094fae78
AngelEscudero4 <angel.escudero4@hotmail.com>
2023-11-08 16:56:52 +0100

    Basic landing with primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,35 @@
-import { NgModule } from '@angular/core';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { DropdownComponent } from './components/dropdown/dropdown.component';
 import { ArrowComponent } from './components/arrow/arrow.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ButtonModule} from "primeng/button";
+import {DropdownModule} from "primeng/dropdown";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NotFoundComponent,
-    DropdownComponent,
     ArrowComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule,
+    DropdownModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    NO_ERRORS_SCHEMA
+  ]
 })
 export class AppModule { }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,16 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  disableGame = true;
+export class HomeComponent implements OnInit{
+  languages ?: string[];
+  selectedLanguage1 ?: string;
+  selectedLanguage2 ?: string;
+
+  ngOnInit() {
+    this.languages = ["Español", "English", "Français", "Italiano"];
+  }
 }
