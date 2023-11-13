b3e6f00a16a58b6cdb00ef8ea09557f0faaf892b
AngelEscudero4 <angel.escudero4@hotmail.com>
2023-11-13 09:43:35 +0100

    Add i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,14 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ButtonModule} from "primeng/button";
 import {DropdownModule} from "primeng/dropdown";
 import {FormsModule} from "@angular/forms";
+import {TranslateLoader, TranslateModule, TranslateService} from "@ngx-translate/core";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -21,6 +29,14 @@ import {FormsModule} from "@angular/forms";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     AppRoutingModule,
     ButtonModule,
     DropdownModule,
@@ -32,4 +48,12 @@ import {FormsModule} from "@angular/forms";
     NO_ERRORS_SCHEMA
   ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['es', 'en', 'fr']);
+    translate.setDefaultLang('es');
+
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang && browserLang.match(/es|en|fr/) ? browserLang : translate.getDefaultLang());
+  }
+}
